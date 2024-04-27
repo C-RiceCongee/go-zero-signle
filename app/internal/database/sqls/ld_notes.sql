dbe3c9ab16aaf659af497394b0e2c572db1bc451
小昊 <xiaohao@xiaohaodeMac-mini.local>
2024-04-27 23:35:18 +0800

    feat: update frontend theme toggle

--- app/internal/database/sqls/ld_notes.sql
+++ app/internal/database/sqls/ld_notes.sql
@@ -1,26 +1,29 @@
 /*
  Navicat Premium Data Transfer
-
+ 
  Source Server         : 114.132.251.2
  Source Server Type    : MySQL
  Source Server Version : 80029 (8.0.29)
  Source Host           : 114.132.251.2:18888
  Source Schema         : ld-notes
-
+ 
  Target Server Type    : MySQL
  Target Server Version : 80029 (8.0.29)
  File Encoding         : 65001
-
+ 
  Date: 24/04/2024 21:39:37
-*/
+ */
+SET
+  NAMES utf8mb4;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET
+  FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for ld_notes
 -- ----------------------------
 DROP TABLE IF EXISTS `ld_notes`;
+
 CREATE TABLE `ld_notes` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文章标题',
@@ -29,6 +32,7 @@ CREATE TABLE `ld_notes` (
   `top` tinyint(1) DEFAULT '0' COMMENT '是否置顶',
   `type_id` bigint DEFAULT NULL COMMENT '类型',
   `tag_ids` bigint DEFAULT NULL COMMENT '标签',
+  `reference_by` varchar(300) default NULL COMMENT '被其他文章引用的id+章节',
   `notes_status` tinyint(1) DEFAULT NULL COMMENT '状态',
   `is_recommended` tinyint(1) DEFAULT '1' COMMENT '是否推荐',
   `is_original` tinyint(1) DEFAULT '1' COMMENT '是否原创',
@@ -37,6 +41,7 @@ CREATE TABLE `ld_notes` (
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
   PRIMARY KEY (`id`),
   KEY `note_title_index` (`title`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='博客-文章表';
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '博客-文章表';
 
-SET FOREIGN_KEY_CHECKS = 1;
+SET
+  FOREIGN_KEY_CHECKS = 1;
---
+++ www/blog-frontend/src/app/publish/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Publish: React.FC = () => {
+
+    // 在这里，您可以根据 `id` 值从 API 或数据库获取对应文章的数据
+    // 然后渲染文章详情
+
+    return (
+        <div>
+            新的文章
+        </div>
+    );
+};
+
+export default Publish;
