fdd0e3684968bca246cfe69dcfbc121c8f09a674
小昊 <xiaohao@xiaohaodeMac-mini.local>
2024-05-20 09:59:41 +0800

    feat: "update code" 🚀🚀🚀

--- www/blog-frontend/src/components/LdMenu/components/CollapseMenu/index.tsx
+++ www/blog-frontend/src/components/LdMenu/components/CollapseMenu/index.tsx
@@ -1,50 +1,18 @@
 import { useState } from 'react'
 import { LdMenuItem } from '../../types'
 import Link from 'next/link'
+
 interface ICollapseMenuProps {
 	list: Array<LdMenuItem>
 	changeTheme: () => void
 }
+
 const CollapseMenu: React.FC<ICollapseMenuProps> = props => {
 	const { list, changeTheme } = props
 	const [open, setOpen] = useState<Boolean>(false)
 	return (
 		<div className='collapseMenu text-skin-menu-content  shadow-2xl rounded-b-lg '>
-			<div className='bars' onClick={() => setOpen(!open)}>
-				<span
-					className={`transition-all duration-10 bg-skin-menu-content bread1 w-10 h-1 mt-1 mb-1 block  ${open && 'rotate-45  -translate-x-2 translate-y-2'
-						}`}
-				></span>
-				<span
-					className={`transition-all duration-10 bg-skin-menu-content bread1 w-10 h-1 mt-1 mb-1 block  ${open && 'opacity-0'
-						}`}
-				></span>
-				<span
-					className={`transition-all duration-10 bg-skin-menu-content bread1 w-10 h-1 mt-1 mb-1 block  ${open && `-rotate-45 -translate-x-2 -translate-y-2`
-						}`}
-				></span>
-			</div>
-
-			<div
-				className={`fullScreeMenu  rounded-b-lg transition-top duration-75  w-full  bg-skin-menu-bg left-0  fixed  ${open ? `top-16  backdrop-blur-xl` : `-top-[2000px] opacity-0`
-					}`}
-			>
-				{list.map(v => {
-					return (
-						<Link
-							target={v.menu_target}
-							href={v.menu_url}
-							key={v.id}
-							className='menuItem  text-skin-menu-content   cursor-pointer text-center block m-5 p-3 box-border'
-						>
-							{v.menu_name}
-						</Link>
-					)
-				})}
-				<div className='text-center m-5 p-3 box-border' onClick={changeTheme}>
-					Theme
-				</div>
-			</div>
+			
 		</div>
 	)
 }
--- www/blog-frontend/src/components/LdMenu/index.tsx
+++ www/blog-frontend/src/components/LdMenu/index.tsx
@@ -5,81 +5,77 @@ import { LdMenuItem } from './types'
 import CollapseMenu from './components/CollapseMenu'
 import Link from 'next/link'
 const LdMenu = () => {
-	const [menuList, setMenuList] = useState<Array<LdMenuItem>>([])
-	useEffect(() => {
-		let timer = setTimeout(() => {
-			setMenuList([
-				{
-					id: 1,
-					parent_menu_id: 0,
-					menu_icon: 'fas fa-home',
-					menu_name: '村头',
-					menu_sort: 1,
-					menu_target: '_self',
-					menu_url: '/',
-				},
-				{
-					id: 2,
-					parent_menu_id: 0,
-					menu_icon: 'fas fa-newspaper',
-					menu_name: '文章',
-					menu_sort: 2,
-					menu_target: '_self',
-					menu_url: '/articles',
-				},
-				{
-					id: 3,
-					parent_menu_id: 2,
-					menu_icon: 'fas fa-file-alt',
-					menu_name: '相册',
-					menu_sort: 1,
-					menu_target: '_self',
-					menu_url: '/articles/new',
-				},
-				{
-					id: 4,
-					parent_menu_id: 0,
-					menu_icon: 'fas fa-users',
-					menu_name: '影音集',
-					menu_sort: 3,
-					menu_target: '_self',
-					menu_url: '/community',
-				},
-				{
-					id: 6,
-					parent_menu_id: 4,
-					menu_icon: 'fas fa-comments',
-					menu_name: '留言区',
-					menu_sort: 1,
-					menu_target: '_self',
-					menu_url: '/community/discussions',
-				},
-				{
-					id: 7,
-					parent_menu_id: 4,
-					menu_icon: 'fas fa-comments',
-					menu_name: '摄影',
-					menu_sort: 1,
-					menu_target: '_self',
-					menu_url: '/community/discussions',
-				},
-				{
-					id: 8,
-					parent_menu_id: 4,
-					menu_icon: 'fas fa-comments',
-					menu_name: '发布',
-					menu_sort: 1,
-					menu_target: '_self',
-					menu_url: '/publish',
-				},
-			])
-		}, 300)
-		return () => {
-			clearTimeout(timer)
-		}
-	}, [])
+	const [open, setOpen] = useState(false)
+	const [menuList, setMenuList] = useState<Array<LdMenuItem>>([
+		{
+			id: 1,
+			parent_menu_id: 0,
+			menu_icon: 'fas fa-home',
+			menu_name: '村头',
+			menu_sort: 1,
+			menu_target: '_self',
+			menu_url: '/',
+		},
+		{
+			id: 2,
+			parent_menu_id: 0,
+			menu_icon: 'fas fa-newspaper',
+			menu_name: '文章',
+			menu_sort: 2,
+			menu_target: '_self',
+			menu_url: '/articles',
+		},
+		// {
+		// 	id: 3,
+		// 	parent_menu_id: 2,
+		// 	menu_icon: 'fas fa-file-alt',
+		// 	menu_name: '相册',
+		// 	menu_sort: 1,
+		// 	menu_target: '_self',
+		// 	menu_url: '/articles/new',
+		// },
+		// {
+		// 	id: 4,
+		// 	parent_menu_id: 0,
+		// 	menu_icon: 'fas fa-users',
+		// 	menu_name: '影音集',
+		// 	menu_sort: 3,
+		// 	menu_target: '_self',
+		// 	menu_url: '/community',
+		// },
+		// {
+		// 	id: 6,
+		// 	parent_menu_id: 4,
+		// 	menu_icon: 'fas fa-comments',
+		// 	menu_name: '留言区',
+		// 	menu_sort: 1,
+		// 	menu_target: '_self',
+		// 	menu_url: '/community/discussions',
+		// },
+		// {
+		// 	id: 7,
+		// 	parent_menu_id: 4,
+		// 	menu_icon: 'fas fa-comments',
+		// 	menu_name: '摄影',
+		// 	menu_sort: 1,
+		// 	menu_target: '_self',
+		// 	menu_url: '/community/discussions',
+		// },
+		// {
+		// 	id: 8,
+		// 	parent_menu_id: 4,
+		// 	menu_icon: 'fas fa-comments',
+		// 	menu_name: '发布',
+		// 	menu_sort: 1,
+		// 	menu_target: '_self',
+		// 	menu_url: '/publish',
+		// },
+	])
+
 	const themes = ['skin-dark', 'sink-white']
 	const [currentTheme, setCurrentTheme] = useState(0)
+
+	// 切换主题
 	const changeTheme = () => {
 		if (currentTheme === themes.length - 1) {
 			setCurrentTheme(0)
@@ -114,9 +110,44 @@ const LdMenu = () => {
 					Theme
 				</div>
 			</header>
-			<header className='mobile-menu bg-skin-menu-bg text-skin-menu-content backdrop-blur md:hidden flex justify-between pt-5 pb-5 pl-5 pr-5'>
-				<div className='logo'>LD-村望老弟</div>
-				<CollapseMenu list={menuList} changeTheme={changeTheme}></CollapseMenu>
+			<header className={`mobile-menu bg-skin-menu-bg duration-100 sticky top-0 text-skin-menu-content backdrop-blur md:hidden  ${open ? 'h-lvh fixed' : 'h-[64px]'} overflow-hidden  pt-5 pb-5`}>
+				<div className="top flex justify-between">
+					<div className='logo'>LD-村望老弟</div>
+					<div className='bars' onClick={() => setOpen(!open)}>
+						<span
+							className={`transition-all duration-10 bg-skin-menu-content bread1 w-10 h-1 mt-1 mb-1 block  ${open && 'rotate-45  -translate-x-2 translate-y-2'
+								}`}
+						></span>
+						<span
+							className={`transition-all duration-10 bg-skin-menu-content bread1 w-10 h-1 mt-1 mb-1 block  ${open && 'opacity-0'
+								}`}
+						></span>
+						<span
+							className={`transition-all duration-10 bg-skin-menu-content bread1 w-10 h-1 mt-1 mb-1 block  ${open && `-rotate-45 -translate-x-2 -translate-y-2`
+								}`}
+						></span>
+					</div>
+				</div>
+				<div
+					className={`fullScreeMenu  rounded-b-lg transition-top duration-75  w-full
+					}`}
+				>
+					{menuList.map(v => {
+						return (
+							<Link
+								target={v.menu_target}
+								href={v.menu_url}
+								key={v.id}
+								className='menuItem  text-skin-menu-content   cursor-pointer text-center block m-5 p-3 box-border'
+							>
+								{v.menu_name}
+							</Link>
+						)
+					})}
+					<div className='text-center m-5 p-3 box-border' onClick={changeTheme}>
+						Theme
+					</div>
+				</div>
 			</header>
 		</div>
 	)
